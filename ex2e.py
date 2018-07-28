ab1cf5b4cdf31f1d478ba3fa373ab563dedc0178
1653315gongrui <dxr.158@163.com>
2018-07-28 14:43:38 +0800

    add python code(example on ppt)

---
+++ ex2e.py
@@ -0,0 +1,7 @@
+print"I have some chickens",5+3
+print"hens",5
+print"roosters",3
+print"DO I have more hens than roosters?",5>3
+print"Do I have no less than 2 roosters?",3>=2
+print"Do I really have 5 hens?",5==5
+print"Is the amount of hens as twice as the amount of roosters?",5/3==2
