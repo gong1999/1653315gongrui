0601d4f1001f767f0a2bd8e76cb7ea1c6a321acc
1653315gongrui <dxr.158@163.com>
2018-07-28 18:31:48 +0800

    add python code

---
+++ ex18.py
@@ -0,0 +1,21 @@
+import dog as d
+d1=d.dog()
+d2=d.dog()
+d1.name="Rover"
+d1.owner="Gong Rui"
+d2.name="Dick"
+d2.owner="Richard"
+print "d1.name,d1.owner,d1.status,d1.mitochondria,d1.spine,d1.lactation:"
+print d1.name,d1.owner,d1.status,d1.mitochondria,d1.spine,d1.lactation
+d1.say1()
+d1.say2()
+d1.say3()
+d1.say4()
+d1.say5()
+print "d2.name,d2.owner,d2.status,d2.mitochondria,d2.spine,d2.lactation:"
+print d2.name,d2.owner,d2.status,d2.mitochondria,d2.spine,d2.lactation
+d2.say1()
+d2.say2()
+d2.say3()
+d2.say4()
+d2.say5()
