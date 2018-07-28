aa317c5428f52f81a3167c93ffa9fa2a63f4e050
1653315gongrui <dxr.158@163.com>
2018-07-28 19:14:23 +0800

    add python code(for composition)

---
+++ composition.py
@@ -0,0 +1,13 @@
+class parent(object):
+    def fun(self):
+        print "parent fun()"
+class child(object):
+    def __init__(self):
+        self.parent=parent()
+    def fun(self):
+        self.parent.fun()
+
+dad=parent()
+son=child()
+dad.fun()
+son.fun()
