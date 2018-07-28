3f78acbb144655a90991e751b3cca370cb168d17
1653315gongrui <dxr.158@163.com>
2018-07-28 14:53:22 +0800

    add python code(example on ppt)

---
+++ ex5e.py
@@ -0,0 +1,17 @@
+x="I like you."
+y="You like me."
+print x+y
+print x*2
+print y*3
+
+a1="g"
+a2="o"
+a3="o"
+a4="d"
+print a1,a2,a3,a4
+print a1+a2+a3+a4
+
+print"I am 5'8\" tall"
+shoppinglist="list:\n\t*cat food\n\t*fish food\n\t*cup cake\n"
+print shoppinglist
+print"\\"
