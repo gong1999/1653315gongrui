1ce17dfe50e08c5a100aa44c3bd6e3857149bf08
1653315gongrui <dxr.158@163.com>
2018-07-28 15:15:11 +0800

    add python code(example on ppt)

---
+++ ex15e.py
@@ -0,0 +1,14 @@
+a=(1,2,3)
+for i in a:
+    print i
+    print i+3
+print "\n"    
+a={0,1,2,3,4,5}
+for i in a:
+    print i
+    print i+3
+print "\n" 
+a={1,2,3}
+b={1:'a',2:'b',3:'c'}
+for i in a:
+    print b[i]
