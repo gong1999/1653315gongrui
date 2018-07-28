ef0946fe1bd513d5a39475a942b4f073faf5a039
1653315gongrui <dxr.158@163.com>
2018-07-28 16:03:45 +0800

    add python code

---
+++ ex17.py
@@ -0,0 +1,3 @@
+import calculate_circumference as cc
+print"The circumference of the Earth's equator is %f m\n"%cc.calculate_circumference(6378)
+print"The circumference of the Martian equator is %f m\n"%cc.calculate_circumference(3396)
