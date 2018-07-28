5d018dc6d5c95f3fc568d4d977d88bafc97f1a3c
1653315gongrui <dxr.158@163.com>
2018-07-28 14:53:02 +0800

    add python code(example on ppt)

---
+++ ex4e.py
@@ -0,0 +1,7 @@
+my_name="Elsa"
+my_age=18
+my_title="queen"
+print"Hello I am %s and I am the %s."%(my_name,my_title)
+print"I am %d years old."%my_age
+print"Hello I am %r and I am the %r."%(my_name,my_title)
+print"I am %r years old."%my_age
