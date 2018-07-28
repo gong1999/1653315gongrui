c201e62308bd620a9aa5043dd5e76de4e4de9b1f
1653315gongrui <dxr.158@163.com>
2018-07-28 15:09:43 +0800

    add python code(example on ppt)

---
+++ ex10e.py
@@ -0,0 +1,10 @@
+def print_two(arg1,arg2):
+    print "I like %r and %r.\n"%(arg1,arg2)
+print_two("apple","banana")
+
+def add(a,b):
+    return a+b
+groupA=20
+groupB=30
+total=add(groupA,groupB)
+print"Total is %d.\n"%total
