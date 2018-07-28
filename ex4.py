a543bd9f4b48d4a7d76372db40948ea5fdd078db
1653315gongrui <dxr.158@163.com>
2018-07-28 09:54:51 +0800

    delete

--- ex4.py
+++
@@ -1,6 +0,0 @@
-#calculate the circumference of the Earth's equator
-pi=3.14#define pi
-r=6378*1000#define the radius of the Earth's equator
-c=2*pi*r#calculate the circumference of the Earth's equator
-print "the circumference of the Earth's equator is %d m"%c#output result
-print "the circumference of the Earth's equator is %.2f m"%c#output result
--- ex5.py
+++
@@ -1,6 +0,0 @@
-#calculate the circumference of the Earth's equator and the Earth's surface area
-pi=3.14#define pi
-r=6378*1000#define the radius of the Earth's equator
-c=2*pi*r#calculate the circumference of the Earth's equator
-s=4*pi*r**2#calculate the Earth's surface area
-print "\n\tthe circumference of the Earth's equator is %d m\n\tthe Earth's surface area is %d m^2\n"%(c,s)#output result
